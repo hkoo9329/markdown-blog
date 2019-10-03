0f046a327f0a4c1f3d6123a60aebc3f80b5be20c
hkoo <tyrfingr12@naver.com>
2019-10-04 00:38:53 +0900

    board,user 도메인 추가

---
+++ src/main/java/com/hkoo/markdownblog/MarkdownBlogApplication.java
@@ -0,0 +1,13 @@
+package com.hkoo.markdownblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkdownBlogApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hkoo/markdownblog/domain/Board.java
@@ -0,0 +1,50 @@
+package com.hkoo.markdownblog.domain;
+
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table
+public class Board {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @Column
+    private String title;
+
+    @Column
+    private String subTitle;
+
+    @Column
+    private String content;
+
+    @Column
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime updatedDate;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Builder
+    public Board(String title, String subTitle, String content, LocalDateTime createdDate,
+                 LocalDateTime updatedDate, User user){
+        this.title = title;
+        this.subTitle = subTitle;
+        this.content = content;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.user = user;
+    }
+}
