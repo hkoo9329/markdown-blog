69106be652c560735288f99de2cf597280fb544f
hkoo <tyrfingr12@naver.com>
2019-10-05 16:43:30 +0900

    기본 화면 및 controller 추가

--- src/main/java/com/hkoo/markdownblog/MarkdownBlogApplication.java
+++ src/main/java/com/hkoo/markdownblog/MarkdownBlogApplication.java
@@ -1,7 +1,16 @@
 package com.hkoo.markdownblog;
 
+import com.hkoo.markdownblog.domain.Board;
+import com.hkoo.markdownblog.domain.User;
+import com.hkoo.markdownblog.repository.BoardRepository;
+import com.hkoo.markdownblog.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class MarkdownBlogApplication {
@@ -9,5 +18,30 @@ public class MarkdownBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(MarkdownBlogApplication.class, args);
     }
+
+
+
+
+//    @Bean
+//    public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) throws Exception {
+//        return (args) -> {
+//            User user = userRepository.save(User.builder()
+//                    .name("havi")
+//                    .password("test")
+//                    .email("havi@gmail.com")
+//                    .createdDate(LocalDateTime.now())
+//                    .build());
+//
+//            IntStream.rangeClosed(1, 200).forEach(index ->
+//                    boardRepository.save(Board.builder()
+//                            .title("게시글"+index)
+//                            .subTitle("순서"+index)
+//                            .content("컨텐츠")
+//                            .createdDate(LocalDateTime.now())
+//                            .updatedDate(LocalDateTime.now())
+//                            .user(user).build())
+//            );
+//        };
+//    }
 
 }
