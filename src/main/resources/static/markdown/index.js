d5b9db654f2241dedb065805fd3ee14e675d87ff
hkoo <tyrfingr12@naver.com>
2019-10-10 01:41:35 +0900

    markdown 라이브러리 변경

--- src/main/resources/static/markdown/index.js
+++ src/main/resources/static/markdown/index.js
@@ -15,17 +15,19 @@ emojify.setConfig({
 });
 
 var md = markdownit({
-        html: true,
-        highlight: function(code, lang) {
-            if (languageOverrides[lang]) lang = languageOverrides[lang];
-            if (lang && hljs.getLanguage(lang)) {
-                try {
-                    return hljs.highlight(lang, code).value;
-                } catch (e) {}
+    html: true,
+    highlight: function (code, lang) {
+        if (languageOverrides[lang]) lang = languageOverrides[lang];
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return hljs.highlight(lang, code).value;
+            } catch (e) {
             }
-            return '';
         }
-    }).use(markdownitFootnote);
+        return '';
+    }
+})
+    .use(markdownitFootnote);
 
 var hashto;
 
@@ -51,7 +53,7 @@ function update(e) {
 }
 
 function setOutput(val) {
-    val = val.replace(/<equation>((.*?\n)*?.*?)<\/equation>/ig, function(a, b) {
+    val = val.replace(/<equation>((.*?\n)*?.*?)<\/equation>/ig, function (a, b) {
         return '<img src="http://latex.codecogs.com/png.latex?' + encodeURIComponent(b) + '" />';
     });
 
@@ -92,11 +94,11 @@ var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
 
 editor.on('change', update);
 
-function selectionChanger(selection,operator,endoperator){
-    if(selection == ""){
+function selectionChanger(selection, operator, endoperator) {
+    if (selection == "") {
         return operator;
     }
-    if(!endoperator){
+    if (!endoperator) {
         endoperator = operator
     }
     var isApplied = selection.slice(0, 2) === operator && seisAppliedection.slice(-2) === endoperator;
@@ -106,29 +108,29 @@ function selectionChanger(selection,operator,endoperator){
 
 editor.addKeyMap({
     // bold
-    'Ctrl-B': function(cm) {
-        cm.replaceSelection(selectionChanger(cm.getSelection(),'**'));
+    'Ctrl-B': function (cm) {
+        cm.replaceSelection(selectionChanger(cm.getSelection(), '**'));
     },
     // italic
-    'Ctrl-I': function(cm) {
-        cm.replaceSelection(selectionChanger(cm.getSelection(),'_'));
+    'Ctrl-I': function (cm) {
+        cm.replaceSelection(selectionChanger(cm.getSelection(), '_'));
     },
     // code
-    'Ctrl-K': function(cm) {
-        cm.replaceSelection(selectionChanger(cm.getSelection(),'`'));
+    'Ctrl-K': function (cm) {
+        cm.replaceSelection(selectionChanger(cm.getSelection(), '`'));
     },
     // keyboard shortcut
-    'Ctrl-L': function(cm) {
-        cm.replaceSelection(selectionChanger(cm.getSelection(),'<kbd>','</kbd>'));
+    'Ctrl-L': function (cm) {
+        cm.replaceSelection(selectionChanger(cm.getSelection(), '<kbd>', '</kbd>'));
     }
 });
 
-document.addEventListener('drop', function(e) {
+document.addEventListener('drop', function (e) {
     e.preventDefault();
     e.stopPropagation();
 
     var reader = new FileReader();
-    reader.onload = function(e) {
+    reader.onload = function (e) {
         editor.setValue(e.target.result);
     };
 
@@ -136,81 +138,76 @@ document.addEventListener('drop', function(e) {
 }, false);
 
 //Print the document named as the document title encoded to avoid strange chars and spaces
-function saveAsMarkdown() {
-    save(editor.getValue(), document.title.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/\s]/gi, '') + ".md");
-}
-
-//Print the document named as the document title encoded to avoid strange chars and spaces
-function saveAsHtml() {
-    save(document.getElementById('out').innerHTML, document.title.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/\s]/gi, '') + ".html");
-}
-
+// function saveAsMarkdown() {
+//     save(editor.getValue(), document.title.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/\s]/gi, '') + ".md");
+// }
+//
+// //Print the document named as the document title encoded to avoid strange chars and spaces
+// function saveAsHtml() {
+//     save(document.getElementById('out').innerHTML, document.title.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/\s]/gi, '') + ".html");
+// }
+//
 // document.getElementById('saveas-markdown').addEventListener('click', function() {
 //     saveAsMarkdown();
 //     hideMenu();
 // });
-
+//
 // document.getElementById('saveas-html').addEventListener('click', function() {
 //     saveAsHtml();
 //     hideMenu();
 // });
 
-function save(code, name) {
-    var blob = new Blob([code], {
-        type: 'text/plain'
-    });
-    if (window.saveAs) {
-        window.saveAs(blob, name);
-    } else if (navigator.saveBlob) {
-        navigator.saveBlob(blob, name);
-    } else {
-        url = URL.createObjectURL(blob);
-        var link = document.createElement("a");
-        link.setAttribute("href", url);
-        link.setAttribute("download", name);
-        var event = document.createEvent('MouseEvents');
-        event.initMouseEvent('click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
-        link.dispatchEvent(event);
-    }
-}
+// function save(code, name) {
+//     var blob = new Blob([code], {
+//         type: 'text/plain'
+//     });
+//     url = URL.createObjectURL(blob);
+//     var link = document.createElement("a");
+//     link.setAttribute("href", url);
+//     link.setAttribute("download", name);
+//     var event = document.createEvent('MouseEvents');
+//     event.initMouseEvent('click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+//     link.dispatchEvent(event);
+//
+// }
 
-var menuVisible = false;
-var menu = document.getElementById('menu');
+// var menuVisible = false;
+// var menu = document.getElementById('menu');
+//
+// function showMenu() {
+//     menuVisible = true;
+//     menu.style.display = 'block';
+// }
+//
+// function hideMenu() {
+//     menuVisible = false;
+//     menu.style.display = 'none';
+// }
 
-function showMenu() {
-    menuVisible = true;
-    menu.style.display = 'block';
-}
-
-function hideMenu() {
-    menuVisible = false;
-    menu.style.display = 'none';
-}
-
-function openFile(evt) {
-    if (window.File && window.FileReader && window.FileList && window.Blob) {
-        var files = evt.target.files;
-        console.log(files);
-        var reader = new FileReader();
-        reader.onload = function(file) {
-            console.log(file.target.result);
-            editor.setValue(file.target.result);
-            return true;
-        };
-        reader.readAsText(files[0]);
-
-    } else {
-        alert('The File APIs are not fully supported in this browser.');
-    }
-}
-
+// function openFile(evt) {
+//     if (window.File && window.FileReader && window.FileList && window.Blob) {
+//         var files = evt.target.files;
+//         console.log(files);
+//         var reader = new FileReader();
+//         reader.onload = function(file) {
+//             console.log(file.target.result);
+//             editor.setValue(file.target.result);
+//             return true;
+//         };
+//         reader.readAsText(files[0]);
+//
+//     } else {
+//         alert('The File APIs are not fully supported in this browser.');
+//     }
+// }
+//
 // document.getElementById('close-menu').addEventListener('click', function() {
 //     hideMenu();
 // });
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
     if (e.keyCode == 83 && (e.ctrlKey || e.metaKey)) {
-        if ( localStorage.getItem('content') == editor.getValue() ) {
+        if (localStorage.getItem('content') == editor.getValue()) {
             e.preventDefault();
             return false;
         }
@@ -232,28 +229,28 @@ function clearEditor() {
     editor.setValue("");
 }
 
-function saveInBrowser() {
-    var text = editor.getValue();
-    if (localStorage.getItem('content')) {
-        swal({
-                title: "Existing Data Detected",
-                text: "You will overwrite the data previously saved!",
-                type: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#DD6B55",
-                confirmButtonText: "Yes, overwrite!",
-                closeOnConfirm: false
-            },
-            function() {
-                localStorage.setItem('content', text);
-                swal("Saved", "Your Document has been saved.", "success");
-            });
-    } else {
-        localStorage.setItem('content', text);
-        swal("Saved", "Your Document has been saved.", "success");
-    }
-    console.log("Saved");
-}
+// function saveInBrowser() {
+//     var text = editor.getValue();
+//     if (localStorage.getItem('content')) {
+//         swal({
+//                 title: "Existing Data Detected",
+//                 text: "You will overwrite the data previously saved!",
+//                 type: "warning",
+//                 showCancelButton: true,
+//                 confirmButtonColor: "#DD6B55",
+//                 confirmButtonText: "Yes, overwrite!",
+//                 closeOnConfirm: false
+//             },
+//             function() {
+//                 localStorage.setItem('content', text);
+//                 swal("Saved", "Your Document has been saved.", "success");
+//             });
+//     } else {
+//         localStorage.setItem('content', text);
+//         swal("Saved", "Your Document has been saved.", "success");
+//     }
+//     console.log("Saved");
+// }
 
 function toggleNightMode(button) {
     button.classList.toggle('selected');
@@ -288,7 +285,7 @@ function processQueryParams() {
     }
     if (params) {
         var obj = {};
-        params.split('&').forEach(function(elem) {
+        params.split('&').forEach(function (elem) {
             obj[elem.split('=')[0]] = elem.split('=')[1];
         });
         if (obj.reading === 'false') {
@@ -301,34 +298,13 @@ function processQueryParams() {
 }
 
 function start() {
-    processQueryParams();
-    if (window.location.hash) {
-        var h = window.location.hash.replace(/^#/, '');
-        if (h.slice(0, 5) == 'view:') {
-            setOutput(decodeURIComponent(escape(RawDeflate.inflate(atob(h.slice(5))))));
-            document.body.className = 'view';
-        } else {
-            editor.setValue(
-                decodeURIComponent(escape(
-                    RawDeflate.inflate(
-                        atob(
-                            h
-                        )
-                    )
-                ))
-            );
-        }
-    } else if (localStorage.getItem('content')) {
-        editor.setValue(localStorage.getItem('content'));
-    }
     update(editor);
     editor.focus();
-    document.getElementById('fileInput').addEventListener('change', openFile, false);
 }
 
 window.addEventListener("beforeunload", function (e) {
     var confirmationMessage = 'It looks like you have been editing something. '
-                            + 'If you leave before saving, your changes will be lost.';
+        + 'If you leave before saving, your changes will be lost.';
     (e || window.event).returnValue = confirmationMessage; //Gecko + IE
     return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
 });
