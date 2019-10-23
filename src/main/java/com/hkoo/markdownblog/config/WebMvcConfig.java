feb894460fbda58177b80fb1376aebbe896816cf
hkoo <tyrfingr12@naver.com>
2019-10-23 16:19:37 +0900

    multipartResolver 설정 추가

---
+++ src/main/java/com/hkoo/markdownblog/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.hkoo.markdownblog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class WebMvcConfig {
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver(){
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        commonsMultipartResolver.setDefaultEncoding("UTF-8");
+        commonsMultipartResolver.setMaxUploadSizePerFile(10 * 1024 * 1024); // max 10mb
+        return commonsMultipartResolver;
+    }
+}
