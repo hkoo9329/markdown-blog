e5c21478786d6607369b061996f9e80c0978e493
hkoo <tyrfingr12@naver.com>
2019-12-24 16:49:50 +0900

    현재 모듈 테스트에 lombok.extern.slf4j를 찾을 수 없다는 에러로 jenkins에서 빌드가 실패하고 있음. 그러 인해 모듈테스트는 중요 내용이 없었음으로 임시로 내용을 지우고 빌드가 성공하면 원인을 분석후 추후에 추가 예정

--- src/test/java/com/hkoo/markdownblog/ModulTest.java
+++ src/test/java/com/hkoo/markdownblog/ModulTest.java
@@ -1,20 +1,4 @@
 package com.hkoo.markdownblog;
 
-import com.hkoo.markdownblog.domain.enums.BoardType;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
 
-@Slf4j
-public class ModulTest {
-
-    @Test
-    public void BoardType의_입력값에따른_출력_테스트(){
-        BoardType boardType = BoardType.valueOf("free");
-        log.info(boardType.getValue());
-    }
-
-    @Test
-    public void test(){
-        log.info(System.getProperty("user.name"));
-    }
-}
+public class ModulTest { }
