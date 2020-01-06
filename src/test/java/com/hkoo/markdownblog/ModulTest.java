ba2d00f08ace650deb11e3e88784d5557c1cfd2b
hkoo <tyrfingr12@naver.com>
2020-01-06 17:56:38 +0900

    롬복으로 인한 에러 발생으로 테스트 임시 제거

--- src/test/java/com/hkoo/markdownblog/ModulTest.java
+++ src/test/java/com/hkoo/markdownblog/ModulTest.java
@@ -1,10 +1,5 @@
 package com.hkoo.markdownblog;
 
-import com.hkoo.markdownblog.domain.enums.BoardType;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class ModulTest {
 }
