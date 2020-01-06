9c468c8065d9380f74211a0487074f1774553900
hkoo <tyrfingr12@naver.com>
2020-01-06 17:55:13 +0900

    롬복으로 인한 에러 발생으로 테스트 임시 제거

--- src/test/java/com/hkoo/markdownblog/ModulTest.java
+++ src/test/java/com/hkoo/markdownblog/ModulTest.java
@@ -5,13 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-@Slf4j
-public class ModulTest {
 
-    @Test
-    public void BoardType생성_테스트(){
-        String boardType;
-        boardType = BoardType.valueOf("notice").getValue();
-        log.info(boardType);
-    }
+public class ModulTest {
 }
